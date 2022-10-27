b491bdd6b73cf3d312e21a02554533c6af8116c2
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 10:08:36 -0300

    feat: classe conta corrente com as variaveis agencia e numero

---
+++ ContaCorrente.java
@@ -0,0 +1,23 @@
+public class ContaCorrente {
+    private int agencia;
+    private int numeroDaConta;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    /**
+     * @param numeroDaConta
+     */
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+}
