807c7fcc613df4d7a4e03496929ba92e0b7281c9
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 14:28:53 -0300

    feat: método de impressão de extrato

--- Conta.java
+++ Conta.java
@@ -41,4 +41,9 @@ public abstract class Conta implements IConta {
         contaDestino.depositar(valor);
     }
 
+    protected void imprimirExtrato() {
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Número: %d", this.numeroDaConta));
+        System.out.println(String.format("Saldo atual da conta: %2f", this.saldo));
+    }
 }
