ae4e138564a5f3df3c83cfbf9ddd65416e6575bf
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 10:10:20 -0300

    feat: classe conta corrente com as variaveis agencia, numero da conta e saldo

---
+++ ContaPoupanca.java
@@ -0,0 +1,32 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numeroDaConta;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    /**
+     * @param numeroDaConta
+     */
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
