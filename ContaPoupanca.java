cf8c7faf8b8136569821c533368957d7e361ff03
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 10:31:16 -0300

    feat: encapsulamento da classe e retirando permissão de mudança das variaveis

--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -7,26 +7,11 @@ public class ContaPoupanca {
         return agencia;
     }
 
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
     public int getNumeroDaConta() {
         return numeroDaConta;
     }
 
-    /**
-     * @param numeroDaConta
-     */
-    public void setNumeroDaConta(int numeroDaConta) {
-        this.numeroDaConta = numeroDaConta;
-    }
-
     public double getSaldo() {
         return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
     }
 }
