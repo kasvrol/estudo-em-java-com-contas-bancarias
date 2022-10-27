16909c604ae05f80b5fea66b6dfdfcd07e042115
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 10:30:51 -0300

    feat: encapsulamento da classe e retirando permissão de mudança das variaveis

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -7,26 +7,11 @@ public class ContaCorrente {
         return agencia;
     }
 
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
     public int getNumeroDaConta() {
         return numeroDaConta;
     }
 
-    /**
-     * @param numeroDaConta
-     */
-    public void setNumeroDaConta(int numeroDaConta) {
-        this.numeroDaConta = numeroDaConta;
-    }
-
     public double getSaldo() {
         return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
     }
 }
