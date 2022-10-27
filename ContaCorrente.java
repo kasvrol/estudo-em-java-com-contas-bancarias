26807bfff46ec826bfffb16482c62e2eddb51feb
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-27 10:41:07 -0300

    feat: uso de herança da superclasse Conta

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,17 +1,2 @@
-public class ContaCorrente {
-    private int agencia;
-    private int numeroDaConta;
-    private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumeroDaConta() {
-        return numeroDaConta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
+public class ContaCorrente extends Conta {
 }
--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -1,17 +1,2 @@
 public class ContaPoupanca {
-    private int agencia;
-    private int numeroDaConta;
-    private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumeroDaConta() {
-        return numeroDaConta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
