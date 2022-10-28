e2b854fb2abd2b513cfa3164f2784ca8d7876984
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-28 10:33:35 -0300

    refactor: exclusão da chamada de métodos para refatoração das classes chamadas

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        ContaCorrente contaCorrente = new ContaCorrente();
 
+        ContaPoupanca contaPoupanca = new ContaPoupanca();
+
+        contaCorrente.imprimirExtrato();
+        contaPoupanca.imprimirExtrato();
+    }
 }
